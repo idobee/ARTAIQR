1d177388957825633cd1e14b242643c06e5f9d0a
idobee <ljymygot@daum.net>
2025-10-29 22:54:14 +0900

    fix: Header menu and routing; index.html restored

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Header from './components/Header';
 
+import HomePage from '../pages/HomePage';
 import ExhibitionsPage from '../pages/exhibitionsPAge';
 import ArtworksPage from '../pages/artworksPage';
 import ArtistsPage from '../pages/artistsPage';
 import CurationPage from '../pages/CurationPage';
 import EducationPage from '../pages/educationPage';
 import AICuratorToolPage from '../pages/AICuratorToolPage';
-import HomePage from '../pages/HomePage';
 
 const App: React.FC = () => (
   <>
@@ -20,7 +20,6 @@ const App: React.FC = () => (
         <Route path="/artworks" element={<ArtworksPage />} />
         <Route path="/artists" element={<ArtistsPage />} />
         <Route path="/curation" element={<CurationPage />} />
-        <Route path="/curations" element={<CurationPage />} />
         <Route path="/education" element={<EducationPage />} />
         <Route path="/ai-curator" element={<AICuratorToolPage />} />
         <Route path="*" element={<Navigate to="/" replace />} />
