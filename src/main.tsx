d384a2146745d22eedeea51b1146611c181df3b3
idobee <ljymygot@daum.net>
2025-10-29 22:46:43 +0900

    chore: deploy to GitHub Pages from main

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import { DataProvider } from '../context/DataContext';
-import { AuthProvider } from '../context/AuthContext';
-import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter basename={import.meta.env.BASE_URL}>
-    <AuthProvider>
-      <DataProvider>
-        <App />
-      </DataProvider>
-    </AuthProvider>
+    <App />
   </BrowserRouter>
 );
