191e44c076d8919d211a9e1b9cb659fbae4340b2
idobee <ljymygot@daum.net>
2025-10-29 22:59:40 +0900

    fix: wrap with DataProvider and add routes

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { DataProvider } from '../context/DataContext';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter basename={import.meta.env.BASE_URL}>
-    <App />
+    <DataProvider>
+      <App />
+    </DataProvider>
   </BrowserRouter>
 );
